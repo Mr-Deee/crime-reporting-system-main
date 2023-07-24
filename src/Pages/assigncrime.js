7451d1494f00ad150541be7b304613b8394408b9
MrAmoako <simplecraft1212@gmail.com>
2023-07-24 20:49:07 +0000

    111

--- src/Pages/assigncrime.js
+++ src/Pages/assigncrime.js
@@ -1,91 +1,56 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import "../assigncrime.css"
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-
+import React, { useEffect, useState } from "react";
+import db from "../firebase";
+import "./assigncrime.css";
 const Assigncrime = () => {
-  const [user, setUser] = useState(null);
-  const [crimeReports, setCrimeReports] = useState([]);
-  const [policeMentors, setPoliceMentors] = useState([]);
-  const [selectedMentorId, setSelectedMentorId] = useState('');
+  const [items, setItems] = useState([]);
+
   useEffect(() => {
-    // Check if there's a user logged in
-    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
-        setUser(user);
-        fetchCrimeReports();
-        fetchPoliceMentors();
-      } else {
-        setUser(null);
+    // Fetch data from Firestore when the component mounts
+    const fetchData = async () => {
+      try {
+        const collectionRef = db.collection("crimeReports");
+        const snapshot = await collectionRef.get();
+        const itemsData = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setItems(itemsData);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
       }
-    });
+    };
 
-    return () => unsubscribe();
+    fetchData();
   }, []);
-
-  const fetchCrimeReports = async () => {
-    try {
-      // Assuming your crime reports collection is named "crimeReports"
-      const snapshot = await firebase.firestore().collection('crimeReports').get();
-      const crimeReportsData = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-      setCrimeReports(crimeReportsData);
-    } catch (error) {
-      console.error('Error fetching crime reports:', error);
-    }
-  };
-
-  const fetchPoliceMentors = async () => {
-    try {
-      // Assuming your police mentors collection is named "policeMentors"
-      const snapshot = await firebase.firestore().collection('policeMen').get();
-      const policeMentorsData = snapshot.docs.map((doc) => doc.data());
-      setPoliceMentors(policeMentorsData);
-    } catch (error) {
-      console.error('Error fetching police mentors:', error);
-    }
-  };
-
-  const handlePoliceMentorChange = (event) => {
-    const selectedMentorId = event.target.value;
-    // Do something with the selected mentor ID, like updating state or performing an action
-    // For example, you can set it to state and use it to filter the crimeReports array.
-  };
-
-  const filteredCrimeReports = selectedMentorId
-    ? crimeReports.filter((report) => report.policeMentorId === selectedMentorId)
-    : crimeReports;
 
   return (
     <div>
-      {user ? (
-        <div>
-          <h2>Welcome, {user.displayName}!</h2>
-          <div>
-            <h3>Crime Reports</h3>
-            <select onChange={handlePoliceMentorChange}>
-              <option value="">Assign Police Man</option>
-              {policeMentors.map((mentor) => (
-                <option key={mentor.id} value={mentor.id}>
-                  {mentor.name}
-                </option>
-              ))}
-            </select>
-            {filteredCrimeReports.map((report) => (
-              <div key={report.id} className="Card">
-                <h4>{report.title}</h4>
-                <p>{report.description}</p>
-                {/* Add other details of the crime report */}
-              </div>
-            ))}
+      <h2>Reports:</h2>
+
+      {items.map((item) => (
+        <li key={item.id}>
+          <div className="card">
+            <div className="card-content">
+              {item.reporterName}
+              <br></br>
+              {item.reporterPhone}
+              <br></br>
+              {item.victimName}
+              <br></br>
+              {item.victimPhone}
+              <br></br>
+              {item.suspectName}
+              <br></br>
+              {item.location}
+              <br></br>
+              {item.dateTime}
+              <br></br>
+            </div>
           </div>
-        </div>
-      ) : (
-        <p>Please log in to view crime reports.</p>
-      )}
+        </li>
+      ))}
     </div>
   );
 };
-
 
 export default Assigncrime;
--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,17 @@
 import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import { firebaseConfig } from './firebaseConfig';
+import "firebase/compat/firestore";
 
-// Initialize Firebase
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
+const firebaseConfig = {
+  apiKey: "AIzaSyD_ZTkkSPzOrXPA5n_lTcX0U3q16OS-_VU",
+  authDomain: "crime-reporting-system-c4a7b.firebaseapp.com",
+  projectId: "crime-reporting-system-c4a7b",
+  storageBucket: "crime-reporting-system-c4a7b.appspot.com",
+  messagingSenderId: "781871322055",
+  appId: "1:781871322055:web:35597bab711f0f47f0bfe2",
+  measurementId: "G-08E8GHRL5K",
+};
 
-export const auth = firebase.auth();
-export default firebase;
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+export default db;
