d4bd199dafad4f0d9d4b0cc8c24e27e5687fb568
MrAmoako <simplecraft1212@gmail.com>
2023-07-24 15:33:20 +0000

    12 commit

--- src/Pages/sign-in/Signin.js
+++ src/Pages/sign-in/Signin.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Signin.js";
 import firebase from "../../firebase.js";
 import Homepage from "../Homepage.js";
@@ -11,16 +11,28 @@ const SignIn = () => {
   const [email, setEmail] = useState("");
   // const history  = useHistory();("");
   const navigate = useNavigate();
-  // let [authMode] = useState("signin");
-  const handleSignIn = async () => {
-    try {
-      // Sign in with email and password
-      await firebase.auth().signInWithEmailAndPassword(email, password);
-      // User signed in successfully, navigate to the assigned crime page
-      navigate("/assignedcrime");
-    } catch (error) {
-      console.error("Error signing in:", error.message);
-    }
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        // If the user is signed in, navigate to the next page (e.g., dashboard)
+        navigate("/assigncrime");
+      }
+    });
+    // Clean up the listener when the component unmounts
+    return () => unsubscribe();
+  }, [navigate]);
+
+  const handleSignIn = () => {
+    // Replace this with your actual sign-in logic using Firebase authentication
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        // Sign-in successful, onAuthStateChanged will handle navigation
+      })
+      .catch((error) => {
+        // Handle sign-in errors here
+      });
   };
 
   return (
@@ -62,37 +74,3 @@ const SignIn = () => {
   );
 };
 export default SignIn;
-
-// return (
-//   <div className="Auth-form-container">
-//     <form className="Auth-form" noValidate>
-//       <div className="Auth-form-content">
-//         <h3 className="Auth-form-title">Sign In</h3>
-//         <div className="text-center">
-//           Already registered?{" "}
-//           <span className="link-primary" onClick={changeAuthMode}>
-//             Sign In
-//           </span>
-//         </div>
-//         <div className="form-group mt-3">
-//           <label>Full Name</label>
-//           <input
-//             type="email"
-//             className="form-control mt-1"
-//             placeholder="e.g Jane Doe"
-//             required
-//           />
-//         </div>
-//         <div className="form-group mt-3">
-//           <label>Email address</label>
-//           <input
-//             type="email"
-//             className="form-control mt-1"
-//             placeholder="Email Address"
-//             required
-//           />
-//         </div>
-//         <div className="form-group mt-3">
-//           <label>Password</label>
-
-// );
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,10 +1,9 @@
-
-    const firebaseConfig = {
-      apiKey: "AIzaSyD_ZTkkSPzOrXPA5n_lTcX0U3q16OS-_VU",
-      authDomain: "crime-reporting-system-c4a7b.firebaseapp.com",
-      projectId: "crime-reporting-system-c4a7b",
-      storageBucket: "crime-reporting-system-c4a7b.appspot.com",
-      messagingSenderId: "781871322055",
-      appId: "1:781871322055:web:35597bab711f0f47f0bfe2",
-      measurementId: "G-08E8GHRL5K",
-    };
+const firebaseConfig = {
+  apiKey: "AIzaSyD_ZTkkSPzOrXPA5n_lTcX0U3q16OS-_VU",
+  authDomain: "crime-reporting-system-c4a7b.firebaseapp.com",
+  projectId: "crime-reporting-system-c4a7b",
+  storageBucket: "crime-reporting-system-c4a7b.appspot.com",
+  messagingSenderId: "781871322055",
+  appId: "1:781871322055:web:35597bab711f0f47f0bfe2",
+  measurementId: "G-08E8GHRL5K",
+};
