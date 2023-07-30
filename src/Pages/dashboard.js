801d77ebd9823c58bcdc1b6735f60b996f681689
MrAmoako <simplecraft1212@gmail.com>
2023-07-30 00:40:47 +0000

    assigncrime db

---
+++ src/Pages/dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Assigncrime from "./assigncrime";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Assigncrime />
+    </div>
+  );
+};
+
+export default Dashboard;
